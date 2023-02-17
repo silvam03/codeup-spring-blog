f61660ce2f13b9ad1485959ac4698095249facc5
Matias Silva <silvamatias03@gmail.com>
2023-02-17 09:29:08 -0600

    Completed controllers exercise

---
+++ src/main/java/com/codeup/codeupspringblog/HomeController.java
@@ -0,0 +1,14 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String homeController() {
+        return "This is the landing page!";
+    }
+}
---
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -0,0 +1,31 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsId(@PathVariable int id) {
+        return "view an individual post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsCreate() {
+        return "view the form for creating a post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String postsCreate2() {
+        return "create a new post";
+    }
+}
